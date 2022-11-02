5addb0cab101ebc52ad68d7a6859523bf42371ad
JuniorDLR <axel.delarocha11977@est.uca.edu.ni>
2022-11-01 20:07:05 -0600

    Navegacion y pantallas estandar

--- app/src/main/java/ni/edu/uca/msclases/Menu.kt
+++ app/src/main/java/ni/edu/uca/msclases/Menu.kt
@@ -37,6 +37,9 @@ class Menu : Fragment() {
         return fbinding.root
 
     }
+    fun salir(){
+        val num= 1
+    }
 
     private fun iniciar() {
         fbinding.btnHorario.setOnClickListener {
