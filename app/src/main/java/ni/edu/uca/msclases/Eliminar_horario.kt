9334e412615cd9647efe7a874c4a6202dab7d32c
DylanEmilio23 <dylan.diaz14298@est.uca.edu.ni>
2022-11-01 19:56:44 -0600

    Navegacion y fragmento estandar

--- app/src/main/java/ni/edu/uca/msclases/Eliminar_horario.kt
+++ app/src/main/java/ni/edu/uca/msclases/Eliminar_horario.kt
@@ -5,17 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-import ni.edu.uca.msclases.databinding.FragmentMenuBinding
 
-
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class Menu : Fragment() {
-
-    private lateinit var fbinding: FragmentMenuBinding
-
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Eliminar_horario.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Eliminar_horario : Fragment() {
+    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -31,33 +33,23 @@ class Menu : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        fbinding = FragmentMenuBinding.inflate(layoutInflater)
-        iniciar()
-        return fbinding.root
-
-    }
-
-    private fun iniciar() {
-        fbinding.btnHorario.setOnClickListener {
-            Navigation.findNavController(fbinding.root).navigate(R.id.horario)
-        }
-        fbinding.btnApuntes.setOnClickListener {
-            Navigation.findNavController(fbinding.root).navigate(R.id.apunte)
-
-        }
-        fbinding.btnContactos.setOnClickListener {
-            Navigation.findNavController(fbinding.root).navigate(R.id.contacto)
-        }
-        fbinding.btnRecursos.setOnClickListener {
-            Navigation.findNavController(fbinding.root).navigate(R.id.recurso)
-        }
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_eliminar_horario, container, false)
     }
 
     companion object {
-
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment Eliminar_horario.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Menu().apply {
+            Eliminar_horario().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
