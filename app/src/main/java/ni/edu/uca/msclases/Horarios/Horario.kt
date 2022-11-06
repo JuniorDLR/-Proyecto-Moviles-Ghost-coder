90372c7e7515e55f6b0aa5733f85a7092cef2d09
DylanEmilio23 <95322178+DylanEmil@users.noreply.github.com>
2022-11-06 16:06:48 -0600

    Actualizacion del Horario

--- app/src/main/java/ni/edu/uca/msclases/Horarios/Horario.kt
+++ app/src/main/java/ni/edu/uca/msclases/Horarios/Horario.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import ni.edu.uca.msclases.R
+import ni.edu.uca.msclases.databinding.FragmentHorarioBinding
 
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class Horario : Fragment() {
+    private lateinit var binding: FragmentHorarioBinding
 
 
     override fun onCreateView(
@@ -19,8 +22,18 @@ class Horario : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_horario, container, false)
-    }
+        binding = FragmentHorarioBinding.inflate(inflater, container, false)
+        return binding.root
 
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnEditarHorario.setOnClickListener {
+            it.findNavController().navigate(R.id.editar_horario)
+        }
+        binding.btnAgregarHorario.setOnClickListener{
+            it.findNavController().navigate(R.id.agregarHorario)
+        }
+    }
 
 }
--- app/src/main/java/ni/edu/uca/msclases/MainActivity.kt
+++ app/src/main/java/ni/edu/uca/msclases/MainActivity.kt
@@ -2,11 +2,14 @@ package ni.edu.uca.msclases
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ni.edu.uca.msclases.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //Esto es una prueba
     }
